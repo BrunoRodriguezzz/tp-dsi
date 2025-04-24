4001315e6cb8e707f53a3b89a759065e49e00914
Valentin Ennio Bravo <vbravo@frba.utn.edu.ar>
2025-04-24 15:50:18 -0300

    Correcciones visuales TestColecciones: Given-When-Then

--- src/test/java/TestColecciones.java
+++ src/test/java/TestColecciones.java
@@ -15,53 +15,70 @@ import static org.mockito.Mockito.when;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TestColecciones {
+    // ------------------------------------------------ Instanciacion de Hechos ------------------------------------------------
     Ubicacion ubicacion1 = new Ubicacion("-36.868375", "-60.343297");
     Categoria categoria1 = new Categoria("Caída de aeronave");
     Origen origen1 = Origen.MANUAL;
-
     Hecho hecho1 = new Hecho("Caída de aeronave impacta en Olavarría",
             "Grave caída de aeronave ocurrió en las inmediaciones de Olavarría, Buenos Aires. El incidente provocó pánico entre los residentes locales. Voluntarios de diversas organizaciones se han sumado a las tareas de auxilio.",
-            categoria1, ubicacion1, LocalDate.of(2001, 11, 21), origen1);
+            categoria1,
+            ubicacion1,
+            LocalDate.of(2001, 11, 21),
+            origen1
+    );
 
     Ubicacion ubicacion2 = new Ubicacion("-37.345571", "-70.241485");
     Categoria categoria2 = new Categoria("Accidente con maquinaria industrial");
     Origen origen2 = Origen.MANUAL;
-
     Hecho hecho2 = new Hecho("Serio incidente: Accidente con maquinaria industrial en Chos Malal, Neuquén",
             "Un grave accidente con maquinaria industrial se registró en Chos Malal, Neuquén. El incidente dejó a varios sectores sin comunicación. Voluntarios de diversas organizaciones se han sumado a las tareas de auxilio.",
-            categoria2, ubicacion2, LocalDate.of(2001, 8, 16), origen2);
+            categoria2,
+            ubicacion2,
+            LocalDate.of(2001, 8, 16),
+            origen2
+    );
 
     Ubicacion ubicacion3 = new Ubicacion("-33.768051", "-61.921032");
     Categoria categoria3 = new Categoria("Caída de aeronave");
     Origen origen3 = Origen.MANUAL;
-
     Hecho hecho3 = new Hecho("Serio incidente: Accidente con maquinaria industrial en Chos Malal, Neuquén",
             "Un grave accidente con maquinaria industrial se registró en Chos Malal, Neuquén. El incidente dejó a varios sectores sin comunicación. Voluntarios de diversas organizaciones se han sumado a las tareas de auxilio.",
-            categoria3, ubicacion3, LocalDate.of(2008, 8, 8), origen3);
+            categoria3,
+            ubicacion3,
+            LocalDate.of(2008, 8, 8),
+            origen3
+    );
 
     Ubicacion ubicacion4 = new Ubicacion("-35.855811", "-61.940589");
     Categoria categoria4 = new Categoria("Accidente en paso a nivel");
     Origen origen4 = Origen.MANUAL;
-
     Hecho hecho4 = new Hecho("Accidente en paso a nivel deja múltiples daños en Pehuajó, Buenos Aires",
             "Grave accidente en paso a nivel ocurrió en las inmediaciones de Pehuajó, Buenos Aires. El incidente generó preocupación entre las autoridades provinciales. El Ministerio de Desarrollo Social está brindando apoyo a los damnificados.",
-            categoria4, ubicacion4, LocalDate.of(2020, 1, 27), origen4);
+            categoria4,
+            ubicacion4,
+            LocalDate.of(2020, 1, 27),
+            origen4
+    );
 
     Ubicacion ubicacion5 = new Ubicacion("-26.780008", "-60.458782");
     Categoria categoria5 = new Categoria("Derrumbe en obra en construcción");
     Origen origen5 = Origen.MANUAL;
-
     Hecho hecho5 = new Hecho("Devastador Derrumbe en obra en construcción afecta a Presidencia Roque Sáenz Peña",
             "Un grave derrumbe en obra en construcción se registró en Presidencia Roque Sáenz Peña, Chaco. El incidente generó preocupación entre las autoridades provinciales. El intendente local se ha trasladado al lugar para supervisar las operaciones.",
-            categoria5, ubicacion5, LocalDate.of(2016, 6, 4), origen5);
+            categoria5,
+            ubicacion5,
+            LocalDate.of(2016, 6, 4),
+            origen5
+    );
 
+
+    // ------------------------------------------------ Instanciacion de Coleccion ------------------------------------------------
     List<Hecho> hechos = Arrays.asList(hecho1, hecho2, hecho3, hecho4, hecho5);
-
-    Fuente fuenteMockeada = mock(Fuente.class);
-
     Coleccion coleccion;
-
     Criterio criterioPruebas;
+
+    // ------------------------------------------------ Instanciacion de Fuente ------------------------------------------------
+    Fuente fuenteMockeada = mock(Fuente.class);
 
     @BeforeEach
     public void setUp(){
@@ -83,6 +100,7 @@ public class TestColecciones {
     public void pruebaCriterios1(){
         RangoFechas rangoFechasFiltro = new RangoFechas(LocalDate.parse("2000-01-01"), LocalDate.parse("2010-01-01"));
         FiltroFechaAcontecimiento filtroFechas = new FiltroFechaAcontecimiento(rangoFechasFiltro);
+
         criterioPruebas.agregarFiltro(filtroFechas);
         coleccion.setCriterio(criterioPruebas);
 
@@ -93,6 +111,7 @@ public class TestColecciones {
 
 //      Al agregar un nuevo criterio de pertenencia y recalcular la coleccion el segundo ya no debería estar presente
         FiltroCategoria filtroCategoria = new FiltroCategoria(categoria1);
+
         criterioPruebas.agregarFiltro(filtroCategoria);
 
         Assertions.assertEquals(2,coleccion.consultarHechos().size());
@@ -107,6 +126,7 @@ public class TestColecciones {
         FiltroTitulo filtroTitulo = new FiltroTitulo("un titulo");
         FiltroCategoria filtroCategoria = new FiltroCategoria(categoria1);
         ArrayList<Filtro> filtros = new ArrayList<>();
+
         filtros.add(filtroTitulo);
         filtros.add(filtroCategoria);
 
