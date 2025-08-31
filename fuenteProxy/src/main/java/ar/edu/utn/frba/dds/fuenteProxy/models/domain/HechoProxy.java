a654b55e86ab16136ea0415f9c77689b0e22fdc0
BrunoRodriguezzz <brrodriguez@frba.utn.edu.ar>
2025-08-31 11:03:40 -0300

    Arreglo problema de dependencias

--- fuenteProxy/src/main/java/ar/edu/utn/frba/dds/fuenteProxy/models/domain/HechoProxy.java
+++ fuenteProxy/src/main/java/ar/edu/utn/frba/dds/fuenteProxy/models/domain/HechoProxy.java
@@ -1,8 +1,9 @@
 package ar.edu.utn.frba.dds.fuenteProxy.models.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
@@ -10,21 +11,37 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name = "Hecho")
+@Table(name = "hecho")
 public class HechoProxy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // Nuestro ID
+    @Column(nullable = false, name = "externo_id")
     private Long idExterno; // Es el ID que nos da el propietario del Hecho
+    @Column(nullable = false, name = "fuente_id")
     private Long idFuente; // Es el ID de la fuente que es propietaria del hecho
+    @Column(nullable = false)
     private String titulo;
+    @Column
     private String descripcion;
+    @Column(nullable = false)
     private String categoria;
+    @Column(nullable = false)
     private Ubicacion ubicacion;
+    @Column(nullable = false)
     private LocalDate fechaHecho;
+    @Column(nullable = false)
     private LocalDateTime fechaCreacion;
+    @Column
     private LocalDateTime fechaModificacion;
+    @Column(nullable = false)
     private Boolean eliminado;
+    @Column(nullable = false)
     private Origen origen;
+    @Column
     private String nombreFuente;
 
     public HechoProxy(Long id, String titulo) {
