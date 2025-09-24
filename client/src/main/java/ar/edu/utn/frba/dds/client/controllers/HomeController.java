e64d7710658b7cd93e3fcc23a9d12e4fe848697c
Alaparamos <alaparamos@frba.utn.edu.ar>
2025-09-24 03:55:23 -0300

    Renderización de Landing Page - TODO: consumir hechos y colecciones destacadas

--- client/src/main/java/ar/edu/utn/frba/dds/client/controllers/HomeController.java
+++ client/src/main/java/ar/edu/utn/frba/dds/client/controllers/HomeController.java
@@ -1,4 +1,16 @@
 package ar.edu.utn.frba.dds.client.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("titulo", "Sistema de Mapeo Colaborativo");
+
+        return "landingPage/index";
+    }
 }
