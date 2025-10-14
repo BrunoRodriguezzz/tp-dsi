b2bfe82a32bb131ec7349224695d9c92cd4f9585
Fernando17A <fernandochoque@frba.utn.edu.ar>
2025-10-14 13:02:20 -0300

    fix

--- client/src/main/java/ar/edu/utn/frba/dds/client/controllers/PublicarHechoController.java
+++ client/src/main/java/ar/edu/utn/frba/dds/client/controllers/PublicarHechoController.java
@@ -1,13 +1,10 @@
 package ar.edu.utn.frba.dds.client.controllers;
 
-import ar.edu.utn.frba.dds.client.dtos.HechoDTO;
 import ar.edu.utn.frba.dds.client.dtos.HechoInputDTO;
-import ar.edu.utn.frba.dds.client.dtos.UbicacionDTO;
 import ar.edu.utn.frba.dds.client.services.DinamicaService;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
