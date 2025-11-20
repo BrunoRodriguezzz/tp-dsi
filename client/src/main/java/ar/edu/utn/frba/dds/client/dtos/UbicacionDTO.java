80d4d96906857ee81ed5868d9fdfba0a271aa1c7
Valentin Ennio Bravo <vbravo@frba.utn.edu.ar>
2025-11-20 12:41:52 -0300

    Correccion de ubicaciones

--- client/src/main/java/ar/edu/utn/frba/dds/client/dtos/UbicacionDTO.java
+++ client/src/main/java/ar/edu/utn/frba/dds/client/dtos/UbicacionDTO.java
@@ -14,4 +14,39 @@ public class UbicacionDTO {
     private String municipio;
     private String latitud;
     private String longitud;
+
+  public String nombreProvincia() {
+    if (this.provincia == null) {
+      return null;
+    }
+
+    return switch (this.provincia) {
+      case "BUENOS_AIRES" -> "Buenos Aires";
+      case "CABA" -> "Ciudad Autónoma de Buenos Aires";
+      case "CATAMARCA" -> "Catamarca";
+      case "CHACO" -> "Chaco";
+      case "CHUBUT" -> "Chubut";
+      case "CORDOBA" -> "Córdoba";
+      case "CORRIENTES" -> "Corrientes";
+      case "ENTRE_RIOS" -> "Entre Ríos";
+      case "FORMOSA" -> "Formosa";
+      case "JUJUY" -> "Jujuy";
+      case "LA_PAMPA" -> "La Pampa";
+      case "LA_RIOJA" -> "La Rioja";
+      case "MENDOZA" -> "Mendoza";
+      case "MISIONES" -> "Misiones";
+      case "NEUQUEN" -> "Neuquén";
+      case "RIO_NEGRO" -> "Río Negro";
+      case "SALTA" -> "Salta";
+      case "SAN_JUAN" -> "San Juan";
+      case "SAN_LUIS" -> "San Luis";
+      case "SANTA_CRUZ" -> "Santa Cruz";
+      case "SANTA_FE" -> "Santa Fe";
+      case "SANTIAGO_DEL_ESTERO" -> "Santiago del Estero";
+      case "TIERRA_DEL_FUEGO" -> "Tierra del Fuego";
+      case "TUCUMAN" -> "Tucumán";
+      default -> this.provincia;
+    };
+  }
+
 }
