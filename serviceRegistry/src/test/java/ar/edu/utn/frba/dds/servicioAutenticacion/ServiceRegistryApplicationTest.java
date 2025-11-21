1272bb78466cc7130266f56efde93b83bc83c30d
Valentin Ennio Bravo <vbravo@frba.utn.edu.ar>
2025-11-20 22:40:01 -0300

    Service registry

---
+++ serviceRegistry/src/test/java/ar/edu/utn/frba/dds/servicioAutenticacion/ServiceRegistryApplicationTest.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.dds.servicioAutenticacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
