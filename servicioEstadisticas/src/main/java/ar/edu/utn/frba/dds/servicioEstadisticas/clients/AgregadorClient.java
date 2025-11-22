9b26da3595788198df6e3bb224afe9a142991550
BrunoRodriguezzz <brrodriguez@frba.utn.edu.ar>
2025-11-22 17:18:53 -0300

    El servicio de estadistica usa el Service registry, pero no funciona

---
+++ servicioEstadisticas/src/main/java/ar/edu/utn/frba/dds/servicioEstadisticas/clients/AgregadorClient.java
@@ -0,0 +1,44 @@
+package ar.edu.utn.frba.dds.servicioEstadisticas.clients;
+
+import ar.edu.utn.frba.dds.servicioEstadisticas.domain.dtos.ColeccionInputDTO;
+import ar.edu.utn.frba.dds.servicioEstadisticas.domain.dtos.HechoInputDTO;
+import ar.edu.utn.frba.dds.servicioEstadisticas.domain.dtos.SolicitudEliminacionInputDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@FeignClient(name = "agregador", configuration = ErrorConfigFeign.class)
+public interface AgregadorClient {
+    @GetMapping("/colecciones")
+    ResponseEntity<Page<ColeccionInputDTO>> buscarColecciones(@RequestParam(name = "all", required = false) Boolean all);
+
+    @GetMapping("/colecciones/{id}/hechos")
+    ResponseEntity<Page<HechoInputDTO>> buscarHechosColeccion(
+            @PathVariable("id") Long id,
+            @RequestParam(name = "categoria", required = false) String categoria,
+            @RequestParam(name = "fechaAcontecimientoInicio", required = false) LocalDateTime fechaAcontecimientoInicio,
+            @RequestParam(name = "fechaAcontecimientoFin", required = false) LocalDateTime fechaAcontecimientoFin,
+            @RequestParam(name = "titulo", required = false) String titulo,
+            @RequestParam(name = "latitud", required = false) Double latitud,
+            @RequestParam(name = "longitud", required = false) Double longitud,
+            @RequestParam(name = "fechaCargaInicio", required = false) LocalDateTime fechaCargaInicio,
+            @RequestParam(name = "fechaCargaFin", required = false) LocalDateTime fechaCargaFin,
+            @RequestParam(name = "fuente", required = false) String fuente,
+            @RequestParam(name = "all", required = false) Boolean all,
+            Pageable pageable
+    );
+
+    @GetMapping("/hechos/independientes")
+    ResponseEntity<List<HechoInputDTO>> buscarHechos();
+
+    @GetMapping()
+    ResponseEntity<List<SolicitudEliminacionInputDTO>> buscarSolicitudes();
+}
