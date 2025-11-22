1089ce125bbfb5a30e4b3cb29d678225b1096b04
Valentin Ennio Bravo <vbravo@frba.utn.edu.ar>
2025-11-22 16:11:00 -0300

    muchos hechos en mapa

--- client/src/main/java/ar/edu/utn/frba/dds/client/controllers/MapaController.java
+++ client/src/main/java/ar/edu/utn/frba/dds/client/controllers/MapaController.java
@@ -20,7 +20,7 @@ public class MapaController {
     @GetMapping("/mapa")
     public String mostrarMapa(Model model) {
 //        List<HechoDTO> hechos = hechoService.obtenerHechosAgregador().getContent();
-        List<HechoDTO> hechos = hechoService.obtenerHechos();
+        List<HechoDTO> hechos = hechoService.obtenerHechosMapa();
         model.addAttribute("hechos", hechos);
         model.addAttribute("mapboxToken", "pk.eyJ1IjoiZmVybmFuZG8xN2EiLCJhIjoiY21nbnk3MDg2MXpteTJucHJsdDllNzZuZCJ9.KuFr7I-l2wBE6ONQk3GpGw");
         return "mapa";
